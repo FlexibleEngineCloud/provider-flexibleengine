073fca432c54112a5bd069d5eba77a07c6045db5
gaetanars <29622411+gaetanars@users.noreply.github.com>
2022-12-09 14:52:55 +0100

    Add dedicatedelb

--- apis/dedicatedelb/v1beta1/zz_generated.managedlist.go
+++ apis/dedicatedelb/v1beta1/zz_generated.managedlist.go
@@ -15,3 +15,57 @@ func (l *CertificateList) GetItems() []resource.Managed {
 	}
 	return items
 }
+
+// GetItems of this IPGroupList.
+func (l *IPGroupList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
+
+// GetItems of this ListenerList.
+func (l *ListenerList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
+
+// GetItems of this LoadBalancerList.
+func (l *LoadBalancerList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
+
+// GetItems of this MemberList.
+func (l *MemberList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
+
+// GetItems of this MonitorList.
+func (l *MonitorList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
+
+// GetItems of this PoolList.
+func (l *PoolList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
--- apis/dedicatedelb/v1beta1/zz_member_types.go
+++ apis/dedicatedelb/v1beta1/zz_member_types.go
@@ -13,36 +13,37 @@ import (
 	v1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
 )
 
-type VipObservation struct {
-	DeviceOwner *string `json:"deviceOwner,omitempty" tf:"device_owner,omitempty"`
-
+type MemberObservation struct {
 	ID *string `json:"id,omitempty" tf:"id,omitempty"`
-
-	Status *string `json:"status,omitempty" tf:"status,omitempty"`
-
-	TenantID *string `json:"tenantId,omitempty" tf:"tenant_id,omitempty"`
 }
 
-type VipParameters struct {
+type MemberParameters struct {
 
-	// +kubebuilder:validation:Optional
-	IPAddress *string `json:"ipAddress,omitempty" tf:"ip_address,omitempty"`
+	// +kubebuilder:validation:Required
+	Address *string `json:"address" tf:"address,omitempty"`
 
 	// +kubebuilder:validation:Optional
 	Name *string `json:"name,omitempty" tf:"name,omitempty"`
 
-	// +crossplane:generate:reference:type=github.com/gaetanars/provider-flexibleengine/apis/vpc/v1beta1.Network
+	// +crossplane:generate:reference:type=Pool
 	// +kubebuilder:validation:Optional
-	NetworkID *string `json:"networkId,omitempty" tf:"network_id,omitempty"`
+	PoolID *string `json:"poolId,omitempty" tf:"pool_id,omitempty"`
 
-	// Reference to a Network in vpc to populate networkId.
+	// Reference to a Pool to populate poolId.
 	// +kubebuilder:validation:Optional
-	NetworkIDRef *v1.Reference `json:"networkIdRef,omitempty" tf:"-"`
+	PoolIDRef *v1.Reference `json:"poolIdRef,omitempty" tf:"-"`
 
-	// Selector for a Network in vpc to populate networkId.
+	// Selector for a Pool to populate poolId.
 	// +kubebuilder:validation:Optional
-	NetworkIDSelector *v1.Selector `json:"networkIdSelector,omitempty" tf:"-"`
+	PoolIDSelector *v1.Selector `json:"poolIdSelector,omitempty" tf:"-"`
 
+	// +kubebuilder:validation:Required
+	ProtocolPort *float64 `json:"protocolPort" tf:"protocol_port,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	Region *string `json:"region,omitempty" tf:"region,omitempty"`
+
+	// The IPv4 or IPv6 subnet ID of the subnet in which to access the member
 	// +crossplane:generate:reference:type=github.com/gaetanars/provider-flexibleengine/apis/vpc/v1beta1.VPCSubnet
 	// +kubebuilder:validation:Optional
 	SubnetID *string `json:"subnetId,omitempty" tf:"subnet_id,omitempty"`
@@ -54,53 +55,56 @@ type VipParameters struct {
 	// Selector for a VPCSubnet in vpc to populate subnetId.
 	// +kubebuilder:validation:Optional
 	SubnetIDSelector *v1.Selector `json:"subnetIdSelector,omitempty" tf:"-"`
+
+	// +kubebuilder:validation:Optional
+	Weight *float64 `json:"weight,omitempty" tf:"weight,omitempty"`
 }
 
-// VipSpec defines the desired state of Vip
-type VipSpec struct {
+// MemberSpec defines the desired state of Member
+type MemberSpec struct {
 	v1.ResourceSpec `json:",inline"`
-	ForProvider     VipParameters `json:"forProvider"`
+	ForProvider     MemberParameters `json:"forProvider"`
 }
 
-// VipStatus defines the observed state of Vip.
-type VipStatus struct {
+// MemberStatus defines the observed state of Member.
+type MemberStatus struct {
 	v1.ResourceStatus `json:",inline"`
-	AtProvider        VipObservation `json:"atProvider,omitempty"`
+	AtProvider        MemberObservation `json:"atProvider,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 
-// Vip is the Schema for the Vips API. <no value>
+// Member is the Schema for the Members API. <no value>
 // +kubebuilder:printcolumn:name="READY",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].status"
 // +kubebuilder:printcolumn:name="SYNCED",type="string",JSONPath=".status.conditions[?(@.type=='Synced')].status"
 // +kubebuilder:printcolumn:name="EXTERNAL-NAME",type="string",JSONPath=".metadata.annotations.crossplane\\.io/external-name"
 // +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:scope=Cluster,categories={crossplane,managed,flexibleengine}
-type Vip struct {
+type Member struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-	Spec              VipSpec   `json:"spec"`
-	Status            VipStatus `json:"status,omitempty"`
+	Spec              MemberSpec   `json:"spec"`
+	Status            MemberStatus `json:"status,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 
-// VipList contains a list of Vips
-type VipList struct {
+// MemberList contains a list of Members
+type MemberList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []Vip `json:"items"`
+	Items           []Member `json:"items"`
 }
 
 // Repository type metadata.
 var (
-	Vip_Kind             = "Vip"
-	Vip_GroupKind        = schema.GroupKind{Group: CRDGroup, Kind: Vip_Kind}.String()
-	Vip_KindAPIVersion   = Vip_Kind + "." + CRDGroupVersion.String()
-	Vip_GroupVersionKind = CRDGroupVersion.WithKind(Vip_Kind)
+	Member_Kind             = "Member"
+	Member_GroupKind        = schema.GroupKind{Group: CRDGroup, Kind: Member_Kind}.String()
+	Member_KindAPIVersion   = Member_Kind + "." + CRDGroupVersion.String()
+	Member_GroupVersionKind = CRDGroupVersion.WithKind(Member_Kind)
 )
 
 func init() {
-	SchemeBuilder.Register(&Vip{}, &VipList{})
+	SchemeBuilder.Register(&Member{}, &MemberList{})
 }
