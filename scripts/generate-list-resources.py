22e5237334e8ed296bdc81b85775a10c6c03e515
Mickael Stanislas <contact@mickael-stanislas.com>
2022-12-11 12:00:40 +0100

    Update list-resources format

--- scripts/generate-list-resources.py
+++ scripts/generate-list-resources.py
@@ -28,8 +28,15 @@ for root, dirs, files in os.walk('.work/FlexibleEngineCloud/flexibleengine/docs/
                         # remove \ in line
                         line = line.replace('\\', '')
 
-                        # identify group of resource (ex: flexibleengine_group_resource)
-                        group = line[2:-1].split('_')[1]
+                        # identify group of resource (ex: Elastic Cloud Server (ECS))
+                        # find line contain 'subcategory: "Elastic Cloud Server (ECS)"' and extract group name
+                        group = ''
+                        with open('.work/FlexibleEngineCloud/flexibleengine/docs/resources/'+file) as x:
+                            for l in x:
+                                if l.find('subcategory: "') != -1:
+                                    group = l[14:-2]
+                                    break
+
                         if group not in linesImplementedPrint:
                             linesImplementedPrint[group] = []
 
@@ -61,7 +68,7 @@ print('\nLast update: ' + os.popen('date').read())
 # Calculate percentage of resources implemented
 percentage = (countImplemented /
               (countImplemented + countNotImplemented)) * 100
-print('\n## Resources implemented: ' + str(countImplemented) +
+print('\n## Resources implemented: ' + str(countImplemented) + '/' + str((countImplemented + countNotImplemented)) +
       ' (' + str(round(percentage, 2)) + '%)\n')
 
 # Print resources implemented by group
@@ -69,8 +76,10 @@ for group in linesImplementedPrint:
     # calculate percentage of resources implemented by group
     percentage = (len(linesImplementedPrint[group]) / (
         len(linesImplementedPrint[group]) + len(linesNotImplementedPrint[group]))) * 100
-    print('### ' + group + ' -- :white_check_mark: ' + str(len(linesImplementedPrint[group])) + ' :x: ' + str(
-        len(linesNotImplementedPrint[group])) + ' (' + str(round(percentage, 2)) + '%)\n')
+    print('### ' + group + ' (' + str(round(percentage, 2)) + '%)\n')
+    print('#### Implemented (' +
+          str(len(linesImplementedPrint[group])) + ') -- Not implemented (' +
+          str(len(linesNotImplementedPrint[group])) + ')\n')
     for line in linesImplementedPrint[group]:
         print(line)
     for line in linesNotImplementedPrint[group]:
