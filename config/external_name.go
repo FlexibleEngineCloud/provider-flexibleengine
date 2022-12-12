c67c8bf58c894d2554bf57ee6c9eea9cd6c5163e
gaetanars <29622411+gaetanars@users.noreply.github.com>
2022-12-12 17:46:36 +0100

    Add comment

--- config/external_name.go
+++ config/external_name.go
@@ -660,10 +660,18 @@ var ExternalNameConfigs = map[string]config.ExternalName{
 	/*
 	  >  Auto Scaling (AS)
 	*/
-	"flexibleengine_as_configuration_v1":  config.IdentifierFromProvider,
-	"flexibleengine_as_group_v1":          config.IdentifierFromProvider,
+	// flexibleengine_as_configuration_v1 - Imported using the ID
+	// https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/resources/as_configuration_v1
+	"flexibleengine_as_configuration_v1": config.IdentifierFromProvider,
+	// flexibleengine_as_group_v1 - Imported using the ID
+	// https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/resources/as_group_v1
+	"flexibleengine_as_group_v1": config.IdentifierFromProvider,
+	// flexibleengine_as_policy_v1 - Imported using the ID
+	// https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/resources/as_policy_v1
+	"flexibleengine_as_policy_v1": config.IdentifierFromProvider,
+	// flexibleengine_as_lifecycle_hook_v1 - Imported using the ID
+	// https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/resources/as_lifecycle_hook_v1
 	"flexibleengine_as_lifecycle_hook_v1": TemplatedStringAsIdentifierWithNoName("{{.parameters.sfs_id}}/{{.external_name}}"),
-	"flexibleengine_as_policy_v1":         config.IdentifierFromProvider,
 }
 
 // TemplatedStringAsIdentifierWithNoName uses TemplatedStringAsIdentifier but
