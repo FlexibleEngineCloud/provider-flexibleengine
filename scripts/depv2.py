3c045e42773e3e996c3be7a544ccba96a2406b09
Mickael Stanislas <contact@mickael-stanislas.com>
2022-12-15 15:13:58 +0100

    Update depv2

--- scripts/depv2.py
+++ scripts/depv2.py
@@ -18,23 +18,31 @@ from collections import defaultdict
 
 def main():
 
-    fileToApply = []
 
     if len(sys.argv) != 2:
         print("Usage: python3 dep.py <groupResource>/<resource>")
         exit(1)
 
-    Labels = defaultdict(dict)
+    debug = False
+
+    fileToApply = []
+    Labels = {}
+    # Labels = defaultdict(dict)
     listLabels = {}
     listLabelsRequired = []
 
     groupResource, resource = sys.argv[1].split("/")
 
+    # if file examples/<groupResource>/<resource>.yaml does not exist
+    if not os.path.isfile("examples/" + groupResource + "/" + resource):
+        print("Error: File examples/" + groupResource + "/" + resource + " does not exist")
+        exit(1)
+
     # Read all yaml files in the directory examples/ and decode them
     # List all directory in the directory examples/
     for dir in os.listdir("examples/"):
         # dir is directory
-        if os.path.isdir("examples/" + dir) and dir != "providerconfig" :
+        if os.path.isdir("examples/" + dir) and dir != "providerconfig" and dir != 'storeconfig' :
             # List all files in the directory examples/<dir>
             for file in os.listdir("examples/" + dir):
                 # If the file is a yaml file
@@ -46,13 +54,16 @@ def main():
 
                             for i in y:
                                 if i != None:
-
                                     if "metadata" in i and "labels" in i["metadata"] and "testing.upbound.io/example-name" in i["metadata"]["labels"]:
-                                        if i["metadata"]["labels"]["testing.upbound.io/example-name"] in listLabels and listLabels[i["metadata"]["labels"]["testing.upbound.io/example-name"]] != i["kind"] + "." + i["apiVersion"]:
-                                            print("Error: Duplicate label testing.upbound.io/example-name: " + i["metadata"]["labels"]["testing.upbound.io/example-name"])
-                                            print("Example: " + listLabels[i["metadata"]["labels"]["testing.upbound.io/example-name"]] + " and " + i["kind"] + "." + i["apiVersion"])
-                                            exit(1)
-                                        else:
+                                        # if i["metadata"]["labels"]["testing.upbound.io/example-name"] in listLabels and listLabels[i["metadata"]["labels"]["testing.upbound.io/example-name"]] != i["kind"] + "." + i["apiVersion"]:
+                                        #     print("Error: Duplicate label testing.upbound.io/example-name: " + i["metadata"]["labels"]["testing.upbound.io/example-name"])
+                                        #     print("Example: " + listLabels[i["metadata"]["labels"]["testing.upbound.io/example-name"]] + " and " + i["kind"] + "." + i["apiVersion"])
+                                        #     exit(1)
+                                        # else:
+
+                                            # if Labels[i["kind"] + "." + i["apiVersion"]] == None:
+                                            if i["kind"] + "." + i["apiVersion"] not in Labels:
+                                                Labels[i["kind"] + "." + i["apiVersion"]] = {}
 
                                             Labels[i["kind"] + "." + i["apiVersion"]]["label"] = i["metadata"]["labels"]["testing.upbound.io/example-name"]
                                             Labels[i["kind"] + "." + i["apiVersion"]]["specs"] = []
@@ -67,29 +78,47 @@ def main():
                                                                 listLabelsRequired.append(v["matchLabels"]["testing.upbound.io/example-name"])
 
                             z = y[0]
+                            if z["kind"] + "." + z["apiVersion"] not in Labels:
+                                Labels[z["kind"] + "." + z["apiVersion"]] = {}
 
                             Labels[z["kind"] + "." + z["apiVersion"]]["file"] = "examples/" + dir + "/" + file
 
                         f.close()
 
-    for labelRequire in listLabelsRequired:
-        for kind in Labels:
-            ## if labelRequire equal Labels[kind]["label"]
-            if labelRequire == Labels[kind]["label"]:
-                for lr in listLabelsRequired:
-                    if lr in Labels[kind]["specs"]:
-                        # delete lr in listLabelsRequired
-                        listLabelsRequired.remove(lr)
-                        break
-                break
+    if debug:
+        print("Labels:")
+        print(Labels)
+        # print("listLabels:")
+        # print(listLabels)
+        print("listLabelsRequired:")
+        print(listLabelsRequired)
 
     for labelRequire in listLabelsRequired:
         for kind in Labels:
-            if labelRequire == Labels[kind]["label"]:
-                fileToApply.append(Labels[kind]["file"])
-                break
+            if kind in Labels:
+                if Labels[kind]["label"] in labelRequire:
+                    for s in Labels[kind]["specs"]:
+                        if s not in listLabelsRequired:
+                            listLabelsRequired.append(s)
 
-    
+    for labelRequire in listLabelsRequired:
+        for kind in Labels:
+            if kind in Labels:
+                if Labels[kind]["label"] in labelRequire:
+                    fileToApply.append(Labels[kind]["file"])
+                    break
+
+    if debug:
+        print("Labels:")
+        print(Labels)
+        print("listLabels:")
+        print(listLabels)
+        print("listLabelsRequired:")
+        print(listLabelsRequired)
+        print("fileToApply:")
+        print(fileToApply)
+
+
     # Print the kubectl command to apply all resources
     origin = sys.argv[1]
     fi = ""
