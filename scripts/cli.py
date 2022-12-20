43f8717ed787c4538d1be1374f7cf5dc40c07d6f
Mickael Stanislas <contact@mickael-stanislas.com>
2022-12-20 17:12:33 +0100

    Update scripts python

--- scripts/cli.py
+++ scripts/cli.py
@@ -13,16 +13,190 @@ import yaml
 from yaml.loader import SafeLoader
 import re
 from collections import defaultdict
+import signal
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import Completer, Completion, PathCompleter
 from prompt_toolkit.document import Document
+from prompt_toolkit.key_binding import KeyBindings
 # from prompt_toolkit.contrib.completers import PathCompleter
 
 
-def main():
+def generateListTested():
 
-    if len(sys.argv) != 2:
-        print("Usage: python3 dep.py <groupResource>/<resource>")
+    def load_gvks(path, loader):
+        types = set()
+        for root, _, files in os.walk(path):
+            for f in files:
+                if f.endswith(".yaml"):
+                    with open(os.path.join(root, f)) as s:
+                        for t in yaml.safe_load_all(s):
+                            for gvk in loader(t):
+                                types.add(gvk)
+        return types
+
+    def load_crd_type(t):
+        kind = t["spec"]["names"]["kind"]
+        group = t["spec"]["group"]
+        for v in t["spec"]["versions"]:
+            yield f'{kind}.{group}/{v["name"]}'
+
+    exceptions = {
+        "provider-flexibleengine": {
+            'ProviderConfigUsage.flexibleengine.upbound.io/v1beta1',
+        },
+    }
+
+    try:
+        exception_set = exceptions["provider-flexibleengine"]
+    except KeyError:
+        exception_set = set()
+    known_crd_types = load_gvks("package/crds/", load_crd_type)
+
+    example_types = load_gvks("examples/", lambda t: [] if t is None or not {"kind", "apiVersion"}.issubset(t.keys())
+                              else [f'{t["kind"]}.{t["apiVersion"]}'])
+
+    tested_crd_types = example_types.union(exception_set)
+
+    # for known_crd_types group by group
+    # ProtectionGroup.sdrs.flexibleengine.upbound.io/v1beta1
+    # ProtectionGroup is the kind
+    # sdrs is the group
+    # v1beta1 is the version
+
+    mapPrint = {}
+    for known_crd_type in known_crd_types:
+        # Extract the group from the known_crd_type
+        group = known_crd_type.split(".")[1]
+        # Extract the kind from the known_crd_type
+        kind = known_crd_type.split(".")[0]
+        # If the group is not in the mapPrint
+        if group not in mapPrint:
+            # Add the group as a key and the kind as a value
+            mapPrint[group] = []
+        tested = ":x:"
+        if known_crd_type in example_types:
+            tested = ":white_check_mark:"
+        # Add the kind to the group
+        mapPrint[group].append(f"|{kind}|{known_crd_type}|{tested}|")
+
+    # Write list of resources in file
+    with open('list-tested.md', 'w') as f:
+        f.write('# Resources tested\n')
+        f.write('\nLast update: ' + os.popen('date').read())
+        for group in mapPrint:
+            f.write(f'\n## {group}\n')
+            f.write(f'|Kind|CRD|Tested|\n')
+            f.write(f'|---|---|---|\n')
+            for kind in mapPrint[group]:
+                f.write(kind+'\n')
+    print("> Successfully generated list of resources tested")
+
+
+def generateListResources():
+    # Count number of resources implemented and not implemented
+    countImplemented = 0
+    countNotImplemented = 0
+
+    # array to store lines to print by group
+    # array array
+    linesImplementedPrint = {}
+    linesNotImplementedPrint = {}
+    linesDeprecatedPrint = []
+
+    for root, dirs, files in os.walk('.work/FlexibleEngineCloud/flexibleengine/docs/resources'):
+        for file in files:
+            if file.endswith('.md'):
+                with open('.work/FlexibleEngineCloud/flexibleengine/docs/resources/'+file) as f:
+                    for line in f:
+                        if line.startswith('# flexibleengine'):
+
+                            # remove \ in line
+                            line = line.replace('\\', '')
+
+                            # identify group of resource (ex: Elastic Cloud Server (ECS))
+                            # find line contain 'subcategory: "Elastic Cloud Server (ECS)"' and extract group name
+                            group = ''
+                            with open('.work/FlexibleEngineCloud/flexibleengine/docs/resources/'+file) as x:
+                                for l in x:
+                                    if l.find('subcategory: "') != -1:
+                                        group = l[14:-2]
+                                        break
+
+                            if group not in linesImplementedPrint:
+                                linesImplementedPrint[group] = []
+
+                            if group not in linesNotImplementedPrint:
+                                linesNotImplementedPrint[group] = []
+
+                            # Define var found to check if resource is already implemented
+                            found = False
+                            with open('config/external_name.go') as x:
+                                for l in x:
+
+                                    # if line containe the resource name
+                                    if l.find(line[2:-1]) != -1:
+                                        found = True
+                                        countImplemented += 1
+                                        linesImplementedPrint[group].append(
+                                            '* [x] [' + line[2:-1] + '](https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/resources/' + file[0:-3] + ')')
+                                        break
+
+                            if found == False:
+                                countNotImplemented += 1
+                                linesNotImplementedPrint[group].append(
+                                    '* [ ] [' + line[2:-1] + '](https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/resources/' + file[0:-3] + ')')
+                            break
+
+    # If exist remove group Deprecated
+    if 'Deprecated' in linesImplementedPrint:
+        linesDeprecatedPrint.append(linesImplementedPrint['Deprecated'])
+        countImplemented -= len(linesImplementedPrint['Deprecated'])
+        del linesImplementedPrint['Deprecated']
+
+    if 'Deprecated' in linesNotImplementedPrint:
+        linesDeprecatedPrint.append(linesNotImplementedPrint['Deprecated'])
+        countNotImplemented -= len(linesNotImplementedPrint['Deprecated'])
+        del linesNotImplementedPrint['Deprecated']
+
+    percentageG = (countImplemented /
+                   (countImplemented + countNotImplemented)) * 100
+
+    # Write list of resources in file
+    with open('list-resources.md', 'w') as f:
+        f.write('# Resources\n')
+        f.write('\nLast update: ' + os.popen('date').read())
+        f.write('\n## Resources implemented: ' + str(countImplemented) + '/' + str((countImplemented + countNotImplemented)) +
+                ' (' + str(round(percentageG, 2)) + '%)\n')
+        for group in linesImplementedPrint:
+            percentage = (len(linesImplementedPrint[group]) / (
+                len(linesImplementedPrint[group]) + len(linesNotImplementedPrint[group]))) * 100
+            f.write('### ' + group + ' (' + str(round(percentage, 2)) + '%)\n')
+            f.write('#### Implemented (' +
+                    str(len(linesImplementedPrint[group])) + '/' +
+                    str(len(linesNotImplementedPrint[group]) +
+                        len(linesImplementedPrint[group])) + ')\n')
+            for line in linesImplementedPrint[group]:
+                f.write(line + '\n')
+            for line in linesNotImplementedPrint[group]:
+                f.write(line + '\n')
+            f.write('\n')
+        if len(linesDeprecatedPrint) > 0:
+            f.write('---------------------------------------\n')
+            f.write('## Deprecated\n')
+            for group in linesDeprecatedPrint:
+                for line in group:
+                    f.write(line + '\n')
+                f.write('\n')
+
+    print("> Successfully generated list of resources")
+    print("Implemented " + str(countImplemented) + "/" + str((countImplemented +
+                                                              countNotImplemented)) + " (" + str(round(percentageG, 2)) + "%) resources")
+
+
+def kubectl():
+
+    if len(sys.argv) != 3:
+        print("Usage: python3 cli.py kubectl <groupResource>/<resource>")
         exit(1)
 
     debug = False
@@ -33,7 +207,7 @@ def main():
     listLabelsRequired = []
     listRefsRequired = []
 
-    groupResource, resource = sys.argv[1].split("/")
+    groupResource, resource = sys.argv[2].split("/")
 
     # if file examples/<groupResource>/<resource>.yaml does not exist
     if not os.path.isfile("examples/" + groupResource + "/" + resource + ".yaml"):
@@ -175,8 +349,6 @@ def main():
         sys.exit(1)
 
     if debug:
-        # print("Labels:")
-        # print(Labels)
         print("listLabelsRequired:")
         print(listLabelsRequired)
         print("listRefsRequired:")
@@ -216,7 +388,25 @@ def help():
     print("Example: python3 dep.py vpc/subnet")
 
 
-class CommandCompleter(Completer):
+class CommandCompleterCmd(Completer):
+    def __init__(self):
+        self.path_completer = PathCompleter()
+        self.commands = ["help", "listResources",
+                         "listTested", "kubectl", "quit"]
+
+    def get_completions(self, document, complete_event):
+        if document.text.startswith(".x "):
+            sub_doc = Document(document.text[3:])
+            yield from (Completion(completion.text, completion.start_position, display=completion.display)
+                        for completion
+                        in self.path_completer.get_completions(sub_doc, complete_event))
+        else:
+            yield from (Completion(cmd, -document.cursor_position)
+                        for cmd in self.commands
+                        if cmd.startswith(document.text))
+
+
+class CommandCompleterKubectl(Completer):
     def __init__(self):
         self.path_completer = PathCompleter()
         self.commands = []
@@ -245,20 +435,35 @@ class CommandCompleter(Completer):
 
 
 if __name__ == "__main__":
-
     # If sys.argv is empty prompt to user to enter the argument
     if len(sys.argv) == 1:
-        other_args = {}
+        other_args_cmd = {}
         inputObj = prompt(
-            "Find example to apply (nat/gateway) >>> ", completer=CommandCompleter(), **other_args)
+            "\nWelcome to CLI toolbox\n(press tab for available commands)\nCmd >>> ", completer=CommandCompleterCmd(), **other_args_cmd)
 
         sys.argv.append(inputObj)
         print("")
 
-    if len(sys.argv) != 2 or sys.argv[1] == "--help":
-        help()
-    else:
-        main()
+    # Switch  sys.argv[1]
+    match sys.argv[1]:
+        case "kubectl":
+            if len(sys.argv) == 2:
+                other_args_kubectl = {}
+                inputObj = prompt("Find example to apply (nat/gateway) >>> ",
+                                  completer=CommandCompleterKubectl(), **other_args_kubectl)
+                sys.argv.append(inputObj)
+            kubectl()
+        case "help":
+            help()
+        case "listResources":
+            generateListResources()
+        case "listTested":
+            generateListTested()
+        case "exit" | "quit":
+            exit(0)
+        # default
+        case _:
+            help()
 
     # Exit 0
     exit(0)
--- scripts/generate-list-resources.py
+++ scripts/generate-list-resources.py
@@ -7,133 +7,103 @@
 
 import os
 
-# Count number of resources implemented and not implemented
-countImplemented = 0
-countNotImplemented = 0
+def generateListResources():
+    # Count number of resources implemented and not implemented
+    countImplemented = 0
+    countNotImplemented = 0
 
-# array to store lines to print by group
-# array array
-linesImplementedPrint = {}
-linesNotImplementedPrint = {}
-linesDeprecatedPrint = []
+    # array to store lines to print by group
+    # array array
+    linesImplementedPrint = {}
+    linesNotImplementedPrint = {}
+    linesDeprecatedPrint = []
 
-for root, dirs, files in os.walk('.work/FlexibleEngineCloud/flexibleengine/docs/resources'):
-    for file in files:
-        if file.endswith('.md'):
-            with open('.work/FlexibleEngineCloud/flexibleengine/docs/resources/'+file) as f:
-                for line in f:
-                    if line.startswith('# flexibleengine'):
+    for root, dirs, files in os.walk('.work/FlexibleEngineCloud/flexibleengine/docs/resources'):
+        for file in files:
+            if file.endswith('.md'):
+                with open('.work/FlexibleEngineCloud/flexibleengine/docs/resources/'+file) as f:
+                    for line in f:
+                        if line.startswith('# flexibleengine'):
 
-                        # remove \ in line
-                        line = line.replace('\\', '')
+                            # remove \ in line
+                            line = line.replace('\\', '')
 
-                        # identify group of resource (ex: Elastic Cloud Server (ECS))
-                        # find line contain 'subcategory: "Elastic Cloud Server (ECS)"' and extract group name
-                        group = ''
-                        with open('.work/FlexibleEngineCloud/flexibleengine/docs/resources/'+file) as x:
-                            for l in x:
-                                if l.find('subcategory: "') != -1:
-                                    group = l[14:-2]
-                                    break
+                            # identify group of resource (ex: Elastic Cloud Server (ECS))
+                            # find line contain 'subcategory: "Elastic Cloud Server (ECS)"' and extract group name
+                            group = ''
+                            with open('.work/FlexibleEngineCloud/flexibleengine/docs/resources/'+file) as x:
+                                for l in x:
+                                    if l.find('subcategory: "') != -1:
+                                        group = l[14:-2]
+                                        break
 
-                        if group not in linesImplementedPrint:
-                            linesImplementedPrint[group] = []
+                            if group not in linesImplementedPrint:
+                                linesImplementedPrint[group] = []
 
-                        if group not in linesNotImplementedPrint:
-                            linesNotImplementedPrint[group] = []
+                            if group not in linesNotImplementedPrint:
+                                linesNotImplementedPrint[group] = []
 
-                        # Define var found to check if resource is already implemented
-                        found = False
-                        with open('config/external_name.go') as x:
-                            for l in x:
+                            # Define var found to check if resource is already implemented
+                            found = False
+                            with open('config/external_name.go') as x:
+                                for l in x:
 
-                                # if line containe the resource name
-                                if l.find(line[2:-1]) != -1:
-                                    found = True
-                                    countImplemented += 1
-                                    linesImplementedPrint[group].append(
-                                        '* [x] [' + line[2:-1] + '](https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/resources/' + file[0:-3] + ')')
-                                    break
+                                    # if line containe the resource name
+                                    if l.find(line[2:-1]) != -1:
+                                        found = True
+                                        countImplemented += 1
+                                        linesImplementedPrint[group].append(
+                                            '* [x] [' + line[2:-1] + '](https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/resources/' + file[0:-3] + ')')
+                                        break
 
-                        if found == False:
-                            countNotImplemented += 1
-                            linesNotImplementedPrint[group].append(
-                                '* [ ] [' + line[2:-1] + '](https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/resources/' + file[0:-3] + ')')
-                        break
+                            if found == False:
+                                countNotImplemented += 1
+                                linesNotImplementedPrint[group].append(
+                                    '* [ ] [' + line[2:-1] + '](https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/resources/' + file[0:-3] + ')')
+                            break
 
-# If exist remove group Deprecated
-if 'Deprecated' in linesImplementedPrint:
-    linesDeprecatedPrint.append(linesImplementedPrint['Deprecated'])
-    countImplemented -= len(linesImplementedPrint['Deprecated'])
-    del linesImplementedPrint['Deprecated']
+    # If exist remove group Deprecated
+    if 'Deprecated' in linesImplementedPrint:
+        linesDeprecatedPrint.append(linesImplementedPrint['Deprecated'])
+        countImplemented -= len(linesImplementedPrint['Deprecated'])
+        del linesImplementedPrint['Deprecated']
 
 
-if 'Deprecated' in linesNotImplementedPrint:
-    linesDeprecatedPrint.append(linesNotImplementedPrint['Deprecated'])
-    countNotImplemented -= len(linesNotImplementedPrint['Deprecated'])
-    del linesNotImplementedPrint['Deprecated']
+    if 'Deprecated' in linesNotImplementedPrint:
+        linesDeprecatedPrint.append(linesNotImplementedPrint['Deprecated'])
+        countNotImplemented -= len(linesNotImplementedPrint['Deprecated'])
+        del linesNotImplementedPrint['Deprecated']
 
-percentageG = (countImplemented /
-               (countImplemented + countNotImplemented)) * 100
-# # Print Last update
-# print('\nLast update: ' + os.popen('date').read())
+    percentageG = (countImplemented /
+                (countImplemented + countNotImplemented)) * 100
 
-# # Calculate percentage of resources implemented
-# print('\n## Resources implemented: ' + str(countImplemented) + '/' + str((countImplemented + countNotImplemented)) +
-#       ' (' + str(round(percentage, 2)) + '%)\n')
-
-# # Print resources implemented by group
-# for group in linesImplementedPrint:
-#     # calculate percentage of resources implemented by group
-#     percentage = (len(linesImplementedPrint[group]) / (
-#         len(linesImplementedPrint[group]) + len(linesNotImplementedPrint[group]))) * 100
-#     print('### ' + group + ' (' + str(round(percentage, 2)) + '%)\n')
-#     print('#### Implemented (' +
-#           str(len(linesImplementedPrint[group])) + '/' +
-#           str(len(linesNotImplementedPrint[group]) +
-#               len(linesImplementedPrint[group])) + ')')
-#     for line in linesImplementedPrint[group]:
-#         print(line)
-#     for line in linesNotImplementedPrint[group]:
-#         print(line)
-#     print('\n')
-
-# # Print Deprecated resources
-# if len(linesDeprecatedPrint) > 0:
-#     print('---------------------------------------')
-#     print('## Deprecated\n')
-#     for group in linesDeprecatedPrint:
-#         for line in group:
-#             print(line)
-#         print('\n')
-
-# Write list of resources in file
-with open('list-resources.md', 'w') as f:
-    f.write('# Resources\n')
-    f.write('\nLast update: ' + os.popen('date').read())
-    f.write('\n## Resources implemented: ' + str(countImplemented) + '/' + str((countImplemented + countNotImplemented)) +
-            ' (' + str(round(percentageG, 2)) + '%)\n')
-    for group in linesImplementedPrint:
-        percentage = (len(linesImplementedPrint[group]) / (
-            len(linesImplementedPrint[group]) + len(linesNotImplementedPrint[group]))) * 100
-        f.write('### ' + group + ' (' + str(round(percentage, 2)) + '%)\n')
-        f.write('#### Implemented (' +
-                str(len(linesImplementedPrint[group])) + '/' +
-                str(len(linesNotImplementedPrint[group]) +
-                    len(linesImplementedPrint[group])) + ')\n')
-        for line in linesImplementedPrint[group]:
-            f.write(line + '\n')
-        for line in linesNotImplementedPrint[group]:
-            f.write(line + '\n')
-        f.write('\n')
-    if len(linesDeprecatedPrint) > 0:
-        f.write('---------------------------------------\n')
-        f.write('## Deprecated\n')
-        for group in linesDeprecatedPrint:
-            for line in group:
+    # Write list of resources in file
+    with open('list-resources.md', 'w') as f:
+        f.write('# Resources\n')
+        f.write('\nLast update: ' + os.popen('date').read())
+        f.write('\n## Resources implemented: ' + str(countImplemented) + '/' + str((countImplemented + countNotImplemented)) +
+                ' (' + str(round(percentageG, 2)) + '%)\n')
+        for group in linesImplementedPrint:
+            percentage = (len(linesImplementedPrint[group]) / (
+                len(linesImplementedPrint[group]) + len(linesNotImplementedPrint[group]))) * 100
+            f.write('### ' + group + ' (' + str(round(percentage, 2)) + '%)\n')
+            f.write('#### Implemented (' +
+                    str(len(linesImplementedPrint[group])) + '/' +
+                    str(len(linesNotImplementedPrint[group]) +
+                        len(linesImplementedPrint[group])) + ')\n')
+            for line in linesImplementedPrint[group]:
+                f.write(line + '\n')
+            for line in linesNotImplementedPrint[group]:
                 f.write(line + '\n')
             f.write('\n')
+        if len(linesDeprecatedPrint) > 0:
+            f.write('---------------------------------------\n')
+            f.write('## Deprecated\n')
+            for group in linesDeprecatedPrint:
+                for line in group:
+                    f.write(line + '\n')
+                f.write('\n')
 
-print("> Successfully generated list of resources")
-print("Implemented " + str(countImplemented) + "/" + str((countImplemented +
-      countNotImplemented)) + " (" + str(round(percentageG, 2)) + "%) resources")
+    print("> Successfully generated list of resources")
+    print("Implemented " + str(countImplemented) + "/" + str((countImplemented +
+        countNotImplemented)) + " (" + str(round(percentageG, 2)) + "%) resources")
