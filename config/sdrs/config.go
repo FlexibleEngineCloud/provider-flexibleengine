7dc6a8e9b379e8048b2a163c3f91ff56aad8c446
Mickael Stanislas <contact@mickael-stanislas.com>
2023-01-09 15:50:45 +0100

    Revert "fix(provider): allow empty insecure parameter"
    This reverts commit d63d1a6a455c9205beb0fca6adc42c171bf35a7e.

--- config/sdrs/config.go
+++ config/sdrs/config.go
@@ -8,11 +8,9 @@ import (
 
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
-
-	// flexibleengine_sdrs_drill_v1
-	// https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/resources/sdrs_drill_v1
 	p.AddResourceConfigurator("flexibleengine_sdrs_drill_v1", func(r *config.Resource) {
 
+		// group_id
 		r.References["group_id"] = config.Reference{
 			Type: "ProtectionGroup",
 		}
@@ -21,10 +19,9 @@ func Configure(p *config.Provider) {
 		}
 
 	})
-
-	// flexibleengine_sdrs_protectedinstance_v1
-	// https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/resources/sdrs_protectedinstance_v1
 	p.AddResourceConfigurator("flexibleengine_sdrs_protectedinstance_v1", func(r *config.Resource) {
+
+		// group_id
 		r.References["group_id"] = config.Reference{
 			Type: "ProtectionGroup",
 		}
@@ -34,18 +31,13 @@ func Configure(p *config.Provider) {
 		r.References["primary_subnet_id"] = config.Reference{
 			Type: tools.GenerateType("vpc", "VPCSubnet"),
 		}
-	})
 
-	// flexibleengine_sdrs_protectiongroup_v1
-	// https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/resources/sdrs_protectiongroup_v1
+	})
 	p.AddResourceConfigurator("flexibleengine_sdrs_protectiongroup_v1", func(r *config.Resource) {
 		r.References["source_vpc_id"] = config.Reference{
 			Type: tools.GenerateType("vpc", "VPC"),
 		}
 	})
-
-	// flexibleengine_sdrs_replication_attach_v1
-	// https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/resources/sdrs_replication_attach_v1
 	p.AddResourceConfigurator("flexibleengine_sdrs_replication_attach_v1", func(r *config.Resource) {
 		r.References["instance_id"] = config.Reference{
 			Type: "ProtectedInstance",
@@ -54,15 +46,12 @@ func Configure(p *config.Provider) {
 			Type: "ReplicationPair",
 		}
 	})
-
-	// flexibleengine_sdrs_replication_pair_v1
-	// https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/resources/sdrs_replication_pair_v1
 	p.AddResourceConfigurator("flexibleengine_sdrs_replication_pair_v1", func(r *config.Resource) {
 		r.References["group_id"] = config.Reference{
 			Type: "ProtectionGroup",
 		}
 		r.References["volume_id"] = config.Reference{
-			Type: tools.GenerateType("evs", "BlockStorageVolume"),
+			Type: tools.GenerateType("evs", "BlockStorageVolume	"),
 		}
 	})
 
