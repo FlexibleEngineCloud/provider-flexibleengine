44e18f101b6986999be6f0c12bb7d0d88bb536d7
dmicheneau <47741512+dmicheneau@users.noreply.github.com>
2023-01-19 11:48:47 +0100

    chore(mrs): update resources configuration

--- config/mrs/config.go
+++ config/mrs/config.go
@@ -3,6 +3,8 @@ package mrs
 
 import (
 	"github.com/upbound/upjet/pkg/config"
+
+	"github.com/FrangipaneTeam/provider-flexibleengine/pkg/tools"
 )
 
 // Configure configures individual resources by adding custom ResourceConfigurators.
@@ -10,14 +12,44 @@ func Configure(p *config.Provider) {
 	// flexibleengine_mrs_cluster_v2
 	// https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/resources/mrs_cluster_v2
 	p.AddResourceConfigurator("flexibleengine_mrs_cluster_v2", func(r *config.Resource) {
-		// node_key_pair is sensitive ?
-		r.TerraformResource.Schema["node_key_pair"].Sensitive = true
+		r.UseAsync = true
+		// vpc_id
+		r.References["vpc_id"] = config.Reference{
+			Type: tools.GenerateType("vpc", "VPC"),
+		}
+		// subnet_id
+		r.References["subnet_id"] = config.Reference{
+			Type: tools.GenerateType("vpc", "Subnet"),
+		}
+		// eip_id
+		r.References["eip_id"] = config.Reference{
+			Type: tools.GenerateType("eip", "EIP"),
+		}
+		// public_ip
+		r.References["public_ip"] = config.Reference{
+			Type:      tools.GenerateType("eip", "EIP"),
+			Extractor: tools.GenerateExtractor(true, "address"),
+		}
+		// node_key_pair
+		r.References["node_key_pair"] = config.Reference{
+			Type: tools.GenerateType("compute", "KeyPair"),
+		}
+		// security_group_ids
+		r.References["security_group_ids"] = config.Reference{
+			Type:              tools.GenerateType("networking", "SecGroup"),
+			Extractor:         tools.GenerateExtractor(true, "id"),
+			SelectorFieldName: "SecurityGroupSelector",
+			RefFieldName:      "SecurityGroupRefs",
+		}
 	})
 
 	// flexibleengine_mrs_job_v2
 	// https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/resources/mrs_job_v2
 	p.AddResourceConfigurator("flexibleengine_mrs_job_v2", func(r *config.Resource) {
-
+		// cluster_id
+		r.References["cluster_id"] = config.Reference{
+			Type: tools.GenerateType("mrs", "Cluster"),
+		}
 		r.UseAsync = true
 	})
 
